e414cd6d0b3dc92aacbed8dc02bcdb2dd050f986
Daniel Di Mella <bigbigbyned@gmail.com>
2022-12-17 22:12:57 -0400

    Add signup feature

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -18,30 +18,6 @@ export class TasksService {
     return await this.taskRepository.getTasks(filterDto);
   }
 
-  //private tasks: Task[] = [];
-  //getAllTasks(): Task[] {
-  //return this.tasks;
-  //}
-
-  //getTasksWithFilter(filterDto: GetTasksFilterDto): Task[] {
-  //const { status, search } = filterDto;
-
-  //let tasks = this.getAllTasks();
-
-  //if (status) {
-  //tasks = tasks.filter(task => task.status === status)
-  //}
-
-  //if (search) {
-  //tasks = tasks.filter(task =>
-  //task.title.includes(search) ||
-  //task.description.includes(search)
-  //)
-  //}
-
-  //return tasks;
-  //}
-
   async getTaskById(id: number): Promise<Task> {
     const found = await this.taskRepository.findOneBy({ id });
 
